8d541d0b9f9b6eea13ebddcc6651163b4d501599
Roger de Oliveira Trevizan <roger.trevizan@gmail.com>
2021-05-22 23:20:53 -0300

    added shadow to menu

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg" 
+<nav class="navbar navbar-expand-lg shadow p-3 mb-5" 
      style="background-image: linear-gradient(to bottom right, #76519c, #6d4794) !important;
             padding-left: 1.2rem; ">
 
