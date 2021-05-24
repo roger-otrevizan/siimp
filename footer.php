9a0546a5cabf3c0d2822e8303badbe67ffbbeb99
Roger de Oliveira Trevizan <roger.trevizan@gmail.com>
2021-05-24 14:08:01 -0300

    comments and sources

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 
   <footer class="text-center text-lg-start text-dark">
 
+    <!-- Texto do rodapé que pega o ano automaticamente com PHP  date('Y')-->
     <div class="text-center p-3" style="background-color: #e2dbea">
       © <?php date('Y') ?> Copyright: <i> Roger de Oliveira Trevizan </i>
     </div>
