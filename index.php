42d96d55d9856a255d59fe6593287d0d215434b2
Roger de Oliveira Trevizan <roger.trevizan@gmail.com>
2021-05-24 14:05:58 -0300

    comments and sources

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once 'assets/db/connection.php';
 //Limpa o banco para envio de novo XML
 $clear = mysqli_query($conn, "TRUNCATE TABLE db_siimp.xml");
 
-
+/* MENU */
 include_once 'header.php';
 
 ?>
@@ -18,12 +18,17 @@ include_once 'header.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Bootstrap 5.0.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- JQuery 3.6.0 -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
+    <!-- FontAwesome 4 -->
     <script src="https://use.fontawesome.com/7fa0f12d2b.js"></script>
 
+    <!-- CSS modificado -->
     <link href="assets/css/custom.css" rel="stylesheet">
 
     <title>SIIMP Sistemas</title>
@@ -40,6 +45,7 @@ include_once 'header.php';
 
         <div class="container-fluid">
 
+            <!-- Formulário de ENVIO do XML -->
             <form id="upload_xml_form" method="POST" enctype="multipart/form-data" action="">
 
                 <div class="row">
@@ -63,12 +69,18 @@ include_once 'header.php';
         <h5>
             <center>Resultado</center>
         </h5>
+
+        <!-- Formulário de PESQUISA na tabela -->
         <form method="POST" id="search_form" action="">
+
             <div class="row">
                 <div class="col-md-9"></div>
+
                 <div class="col-md-3 text-end">
                     <div class="input-group rounded">
+
                         <input type="search" name="search" id="search" class="form-control rounded" placeholder="Pesquisar..." aria-label="Search" aria-describedby="search-addon" />
+
                         <span class="input-group-text border-0" id="search-addon">
                             <i class="fa fa-search"></i>
                         </span>
@@ -78,10 +90,14 @@ include_once 'header.php';
             </div>
         </form>
 
+        <!-- Script de pesquisa na tabela -->
         <script src="assets/js/search_table.js"></script>
 
+        <!-- Tabela com resultado do XML -->
         <div class="shadow p-2 mb-3 bg-body rounded">
+
             <table id="table_xml" class="table table-hover" style="text-align: center;">
+
                 <thead class="table table-dark">
                     <tr>
                         <th scope="col">Caminho</th>
@@ -98,8 +114,10 @@ include_once 'header.php';
 
     </div>
 
+    <!-- Script de envio do XML -->
     <script src="assets/js/send_xml.js"></script>
 
+    <!-- Rodapé -->
     <?php
     include_once 'footer.php';
     ?>
